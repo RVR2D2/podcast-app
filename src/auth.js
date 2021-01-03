ecf945b83fb35c1d0dd8c699cd56b1fbf75cb9f7
vadim <poom_peii01@gmail.com>
2021-01-03 13:57:59 +0600

    podcast app

---
+++ src/auth.js
@@ -0,0 +1,32 @@
+export function getAuthForm() {
+    return `
+        <form class="mui-form" id="auth-form">
+             <div class="mui-textfield mui-textfield--float-label">
+                  <input type="email" id="email" required>
+                  <label for="email">Email</label>
+             </div>
+             
+             <div class="mui-textfield mui-textfield--float-label">
+                  <input type="password" id="password" required>
+                  <label for="password">Пароль</label>
+             </div>
+             <button type="submit" class="mui-btn mui-btn--raised mui-btn--primary">Войти</button>
+        </form>
+    `
+}
+
+export function authWidthEmailAndPassword(email, password) {
+    const apiKey = 'AIzaSyBQ-I6LOajJDRB7nLQTrqEdW12vPRitBws'
+    return  fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`,{
+        method: 'POST',
+        body: JSON.stringify({
+            email, password,
+            returnSecureToken: true
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(data => data.idToken)
+}
